f7d100e407fdd65337bb790868ea4dff2904ab06
phleech <phleech@phleech.co.uk>
2025-12-12 01:28:26 +0000

    Day 11, Part 1

---
+++ 2025/day11/day11-1.php
@@ -0,0 +1,45 @@
+<?php
+
+$input = [
+    'aaa: you hhh',
+    'you: bbb ccc',
+    'bbb: ddd eee',
+    'ccc: ddd eee fff',
+    'ddd: ggg',
+    'eee: out',
+    'fff: out',
+    'ggg: out',
+    'hhh: ccc fff iii',
+    'iii: out',
+];
+
+$input = explode("\n", trim(file_get_contents("input.txt")));
+
+$routes = [];
+foreach ($input as $route) {
+    $matches = [];
+    preg_match_all('/^(\w*):\s(.*+)$/', $route, $matches);
+
+    $routes[$matches[1][0]] = explode(' ', $matches[2][0]);
+}
+
+$count = 0;
+foreach ($routes as $name => $devices) {
+    if ($name == 'you') {
+        findRoute($routes, $devices, $count);
+    }
+}
+
+echo "found $count routes\n";
+
+function findRoute($routes, $devices, &$count) {
+    foreach ($devices as $device) {
+        if ($device == 'out') {
+            $count++;
+            return;
+        }
+
+        findRoute($routes, $routes[$device], $count);
+    }
+}
+
